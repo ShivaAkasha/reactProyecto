f229305998ffd2e02fda904d7146846a964e192e
Alumnos Santiago Centro <alumnos09@inacap.cl>
2024-07-11 15:06:32 -0400

    avancejoao

--- app/src/components/FormularioFactura.jsx
+++ app/src/components/FormularioFactura.jsx
@@ -3,7 +3,7 @@ export function FormularioFactura(){
     return(
         <>
         <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <label for="exampleFormControlInput1" class="form-label">NOMBRE USUARIO</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
 </div>
 <div class="mb-3">
